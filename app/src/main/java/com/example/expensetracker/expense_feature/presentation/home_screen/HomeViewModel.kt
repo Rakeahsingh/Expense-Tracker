3e3692d1d9fa72ace3f32de8c9564df1fb42192c
Rakeahsingh <bca02121502019@gmail.com>
2023-12-21 13:07:50 +0530

    hello

--- app/src/main/java/com/example/expensetracker/expense_feature/presentation/home_screen/HomeViewModel.kt
+++ app/src/main/java/com/example/expensetracker/expense_feature/presentation/home_screen/HomeViewModel.kt
@@ -24,14 +24,17 @@ import com.example.expensetracker.expense_feature.domain.use_cases.write_databas
 import com.example.expensetracker.utils.Account
 import com.example.expensetracker.utils.Category
 import com.example.expensetracker.utils.TabButton
+import com.example.expensetracker.utils.TransactionType
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.text.DecimalFormat
 import java.util.Calendar
 import java.util.Date
 import javax.inject.Inject
@@ -101,7 +104,7 @@ class HomeViewModel @Inject constructor(
     var currentTime = MutableStateFlow(Calendar.getInstance().time)
         private set
 
-    var selectedCountryCode = MutableStateFlow(String())
+    var selectedCurrencyCode = MutableStateFlow(String())
         private set
 
     var limitAlert = MutableSharedFlow<UiEvent>(replay = 1)
@@ -211,6 +214,140 @@ class HomeViewModel @Inject constructor(
         }else{
             transactionAmount.value = String.format("%.2f", (whole + amount).toDouble())
         }
+    }
+
+    fun backSpace(){
+        val value = transactionAmount.value
+        var whole = value.substring(0, value.indexOf("."))
+
+        if (value == "0.00"){
+            return
+        }
+
+        if (isDecimal.value){
+            decimal = if (decimal.length == 2){
+                decimal.substring(0, decimal.length - 1)
+            }else{
+                isDecimal.value = false
+                "0"
+            }
+            val newDecimal = decimal.toDouble() / 100.0
+            transactionAmount.value = String.format("%.2f", whole.toDouble() + newDecimal)
+            decimal = String()
+            return
+        }
+        whole = if (whole.length - 1 == 0)
+            "0"
+        else
+            whole.substring(0, whole.length - 1)
+        transactionAmount.value = String.format("%.2f", whole.toDouble())
+    }
+
+    fun displayTransaction(
+        transactionDate: String?,
+        transactionPos: Int?,
+        transactionStatus: Int?
+    ){
+        if (transactionPos != -1 && transactionStatus != -1){
+            val transaction = if (transactionStatus == 0){
+                dailyTransaction.value[transactionPos!!]
+            }else{
+                transactionDate?.let {
+                    monthlyTransaction.value[it]!![transactionPos!!]
+                }
+            }
+
+            setTransactionTitle(transaction!!.title)
+            currentTime.value = transaction.date
+            Account.entries.forEach { account ->
+                if (account.title == transaction.title){
+                    selectAccount(account)
+                    return@forEach
+                }
+            }
+            transactionAmount.value = transaction.amount.toString()
+            Category.entries.forEach { category ->
+                if (category.title == transaction.category){
+                    selectCategory(category)
+                    return@forEach
+                }
+            }
+        }
+    }
+
+    fun updateTransaction(
+        transactionDate: String?,
+        transactionPos: Int?,
+        transactionStatus: Int?,
+        navigateBack: () -> Unit
+    ){
+        if (transactionPos != -1 && transactionStatus != -1){
+            val transaction = if (transactionStatus == 0){
+                dailyTransaction.value[transactionPos!!]
+            }else{
+                transactionDate?.let {
+                    monthlyTransaction.value[it]!![transactionPos!!]
+                }
+            }
+
+            viewModelScope.launch {
+                if (transactionAmount.value.toDouble() != transaction!!.amount){
+                    val currentAccount = getAccountUseCase(account.value.title).first()
+                    if (transaction.transactionType == TransactionType.INCOME.title){
+                        currentAccount.income = currentAccount.income - transaction.amount
+                        currentAccount.income = currentAccount.income + transactionAmount.value.toDouble()
+                        currentAccount.balance = currentAccount.income - currentAccount.expense
+                    }else{
+                        currentAccount.expense = currentAccount.expense - transaction.amount
+                        currentAccount.expense = currentAccount.expense + transactionAmount.value.toDouble()
+                        currentAccount.balance = currentAccount.income - currentAccount.expense
+                    }
+                    insertAccountUseCase(listOf(currentAccount))
+                }
+
+                val updateTransaction = TransactionDto(
+                    transaction.date,
+                    transaction.dateOfEntry,
+                    account.value.title,
+                    transactionAmount.value.toDouble(),
+                    category.value.title,
+                    transaction.transactionType,
+                    transactionTitle.value
+                )
+                insertNewTransactionUseCase(updateTransaction)
+                withContext(Main){
+                    navigateBack()
+                }
+            }
+        }
+    }
+
+    fun displayExpenseLimitWarning(){
+        viewModelScope.launch {
+            getExpenseLimitUseCase().collectLatest { expenseAmount ->
+                if (expenseAmount <= 0) return@collectLatest
+                val threshold = 0.8 * expenseAmount
+                when{
+                    currentExpenseAmount.value > expenseAmount -> {
+                        val expenseOverflow = (currentExpenseAmount.value - expenseAmount).toString().amountFormat()
+                        val info = "${selectedCurrencyCode.value} $expenseOverflow Over Specified Limit"
+                        limitAlert.emit(UiEvent.Alert(info))
+                    }
+                    currentExpenseAmount.value > threshold -> {
+                        val expenseAvailable = (expenseAmount - currentExpenseAmount.value).toString().amountFormat()
+                        val info = "${selectedCurrencyCode.value} $expenseAvailable Away from specified limt"
+                        limitAlert.emit(UiEvent.Alert(info))
+                    }
+                    else -> limitAlert.emit(UiEvent.NoAlert())
+                }
+            }
+        }
+    }
+
+
+    fun String.amountFormat(): String {
+        val amountFormatter = DecimalFormat("#,##0.00")
+        return " " + amountFormatter.format(this.toDouble())
     }
 
 
